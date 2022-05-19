e40dbd272de0ae33bbeb470b80600396e5c00a24
Tran Cao Bao Phuc <62349601+CrazyMeowl@users.noreply.github.com>
2022-05-19 11:15:19 +0700

    Tessst

--- React/server/index.js
+++ React/server/index.js
@@ -1,9 +1,11 @@
-// server/index.js
+// index.js
+
 const path = require('path');
 const express = require('express');
 
-...
+const PORT = process.env.PORT || 3001;
 
+const app = express();
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
@@ -15,4 +17,8 @@ app.get("/api", (req, res) => {
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
 });
